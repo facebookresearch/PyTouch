59683b7eeb7d9ecd6abd7a341bc6a3e47d88bc69
vocdex <nazirjonovsh2000@gmail.com>
2022-07-17 17:08:01 +0900

    adding real-time contact area extraction example

--- examples/contact_area.py
+++ examples/contact_area.py
@@ -29,5 +29,33 @@ def extract_surface_contact():
     ImageHandler.save("surface_contact_2.png", sample_img_2)
 
 
+def extract_surface_contact_real_time():
+    """
+    This function demonstrates how to use the ContactArea task with real-time mode with DIGIT sensor.
+    Make sure to have a DIGIT sensor connected to your computer and setup the digit-interface library.
+    Output:
+        Displays a real-time object contact area by fitting an ellipse.
+    """
+    from digit_interface import Digit
+    import cv2
+    base_img_path = "/home/shuk/digits2/images/0000/0041.png"
+    base_img = ImageHandler(base_img_path).nparray
+    # setting up the DIGIT sensor
+    digit = Digit("Dxxxxx")  # replace xxxxx with your DIGIT serial number
+    digit.connect()
+    while True:
+        frame=digit.get_frame()
+        img=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # target image frame for __call__ method
+        contact_area=ContactArea(base=base_img, contour_threshold=10,real_time=True)
+        contact_area(target=img) # __call__ method
+        cv2.imshow('surface contact', img)
+        k=cv2.waitKey(1)
+        if k%256==27:
+            #ESC hit
+            print("Escape hit, closing...")
+            break
+
+
 if __name__ == "__main__":
-    extract_surface_contact()
+    #extract_surface_contact()
+    extract_surface_contact_real_time()
